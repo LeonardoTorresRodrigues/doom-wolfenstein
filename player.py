bd516c562a109bc4cad86502341cd833fe3d96bf
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-01-24 13:55:58 -0300

    feat: add new textures to the game

--- player.py
+++ player.py
@@ -25,11 +25,11 @@ class Player:
             dx += -speed_cos
             dy += -speed_sin
         if keys[pg.K_a]:
-            dy += speed_sin
-            dx += -speed_cos
+            dx += speed_sin
+            dy += -speed_cos
         if keys[pg.K_d]:
-            dy += -speed_sin
-            dx += speed_cos
+            dx += -speed_sin
+            dy += speed_cos
 
         self.check_wall_collision(dx, dy)
 
@@ -43,9 +43,10 @@ class Player:
         return (x, y) not in self.game.map.world_map
 
     def check_wall_collision(self, dx, dy):
-        if self.check_wall(int(self.x + dx), int(self.y)):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+        if self.check_wall(int(self.x + dx * scale), int(self.y)):
             self.x += dx
-        if self.check_wall(int(self.x), int(self.y + dy)):
+        if self.check_wall(int(self.x), int(self.y + dy * scale)):
             self.y += dy
 
     def draw(self):
